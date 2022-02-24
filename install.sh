071d73ee6e4724ec93693796b363acbdf569e568
¢¥B€R Λ₤ÑØΜ <72166424+venomous-maker@users.noreply.github.com>
2022-02-24 00:07:00 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-
+echo "PACKAGES AND LIBRABRIES INSTALLATION IN PROGRESS Loading...prog1"
+sudo apt-get install python3
+echo "PACKAGES AND LIBRABRIES INSTALLATION IN PROGRESS Loading...prog2"
+clear
+pip install socket
+echo "PACKAGES AND LIBRABRIES INSTALLATION IN PROGRESS Loading...prog4"
+clear
+pip install threading
+echo "PACKAGES AND LIBRABRIES INSTALLATION IN PROGRESS Loading...prog4"
+clear
+pip install subprocess
+echo "PACKAGES AND LIBRABRIES INSTALLATION IN PROGRESS Loading...prog5"
+clear
+pip install colorama
+clear
+echo "PACKAGES AND LIBRABRIES INSTALLATION IN PROGRESS DONE SUCCESSFULLY"
